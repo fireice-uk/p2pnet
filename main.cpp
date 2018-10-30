8ea0de0995b76002db5c1ddfff9f2dcf30b41767
BenqGG <bbarret93@gmail.com>
2018-10-30 20:51:44 +0100

    Auto stash before merge of "master" and "origin/master"

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@
 
 int main()
 {
+<<<<<<< Updated upstream
 	srand(std::time(NULL));
 	wsock_init();
 
@@ -31,6 +32,25 @@ int main()
 
 	if(inter[0] == '0')
 		interptr = nullptr;
+=======
+  srand(std::time(NULL));
+  wsock_init();
+
+  //char inter[128];
+  //char *interptr = inter;
+ // std::cout << "Listen Interface(0 for null): ";
+ // std::cin >> inter;
+
+ // if (inter[0] == '0')
+	//  interptr = nullptr;
+
+  /*
+  int port;
+  std::cout << "Listen Port: ";
+  std::cin >> port;
+  inc_peer_factory incfac;
+  incfac.start(interptr, port);
+>>>>>>> Stashed changes
 
 	int port;
 	std::cout << "Listen Port: ";
@@ -54,6 +74,7 @@ int main()
 	out_peer_factory outfac;
 	outfac.connect_peers(1);
 
+<<<<<<< Updated upstream
 	sleep(0.5f);
 	std::cin.ignore(1024, '\n');
 	std::cout << "Press enter to continue..." << std::endl;
@@ -62,6 +83,21 @@ int main()
 	incfac.stop_peers();
 	incfac.stop();
 	outfac.stop_peers();
+=======
+  incfac.stop_peers();
+  incfac.stop();
+  outfac.stop_peers();*/
+  std::cout << "CONNECTING TO: " << std::endl;
+  std::cout << "PRESS ENTER TO PROCEED" << std::endl;
+  std::cin.get();
+  out_peer_factory outfac;
+  outfac.connect_peers(1);
+  
+  std::cout << "PRESS ENTER TO END CONNECTION" << std::endl;
+  std::cin.get();
+  outfac.stop_peers();
+  wsock_cleaup();
+>>>>>>> Stashed changes
 
 	wsock_cleaup();
 
